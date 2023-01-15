5d0254eeb7470ca020a368f5d665890efb7b8ac0
avisrabon22@gmail.com <avisrabon22@gmail.com>
2023-01-15 15:18:33 +0530

    new pfoject

---
+++ src/main/java/com/shopesimple/invManager/InvManagerApplication.java
@@ -0,0 +1,13 @@
+package com.shopesimple.invManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopesimple/invManager/InvManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopesimple.invManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
