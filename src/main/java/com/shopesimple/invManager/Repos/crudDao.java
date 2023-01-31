caaed98b0036d2e004118495f15c1daf83d43d9f
avisrabon22@gmail.com <avisrabon22@gmail.com>
2023-01-31 20:09:23 +0530

    branch added

--- src/main/java/com/shopesimple/invManager/Repos/crudDao.java
+++ src/main/java/com/shopesimple/invManager/Repos/crudDao.java
@@ -1,11 +1,16 @@
 package com.shopesimple.invManager.Repos;
 
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.shopesimple.invManager.Models.users;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Repository
 public interface crudDao extends MongoRepository<users,String> {
-
-
-    
+  
+       
 } 
