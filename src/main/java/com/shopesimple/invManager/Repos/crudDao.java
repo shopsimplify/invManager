50d0207cb5fd179bcf625c280e059065070a61b4
avisrabon22@gmail.com <avisrabon22@gmail.com>
2023-02-09 11:15:16 +0530

    update

--- src/main/java/com/shopesimple/invManager/Repos/crudDao.java
+++ src/main/java/com/shopesimple/invManager/Repos/crudDao.java
@@ -2,12 +2,8 @@ package com.shopesimple.invManager.Repos;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import com.shopesimple.invManager.Models.users;
 
-@Repository
 public interface crudDao extends MongoRepository<users,String> {
-    
-  
        
 } 
