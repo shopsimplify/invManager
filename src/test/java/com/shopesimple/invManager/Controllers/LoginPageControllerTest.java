7e80eb337131b5b31a723d895545e006cd8b4c1d
avisrabon22 <avisrabon22@gmail.com>
2023-11-10 09:23:24 +0530

    update on test

---
+++ src/test/java/com/shopesimple/invManager/Controllers/LoginPageControllerTest.java
@@ -0,0 +1,15 @@
+package com.shopesimple.invManager.Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginPageControllerTest {
+
+
+    @Test
+    public void productController(){
+
+    }
+
+}
