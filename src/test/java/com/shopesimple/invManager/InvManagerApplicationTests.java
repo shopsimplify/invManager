4049fa702efb102a22a868fe5ad55425d3a603a7
avisrabon22 <avisrabon22@gmail.com>
2023-12-11 11:10:26 +0530

    update on test

--- src/test/java/com/shopesimple/invManager/InvManagerApplicationTests.java
+++ src/test/java/com/shopesimple/invManager/InvManagerApplicationTests.java
@@ -3,10 +3,10 @@ package com.shopesimple.invManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class InvManagerApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
